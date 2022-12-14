abd73f8e23a618a2c091ace30c203196003ed8c5
laindomJS <adrijar21@gmail.com>
2022-12-14 12:42:14 -0400

    fix: fixed fetch error in details page

--- src/hooks/useGetCountryNames.tsx
+++ src/hooks/useGetCountryNames.tsx
@@ -7,10 +7,12 @@ export const useGetCountryNames = (country:CountryT) => {
   const [countryNames, setCountryNames] = useState<CountryT[]>([]);
 
   useEffect(() => {
-    fetch(`https://restcountries.com/v3.1/alpha?codes=${country.borders.join(",")}`)
-      .then(res => res.json())
-      .then(data => setCountryNames(data))
-      .catch(err => console.log(err));
+    (!country.borders) 
+    ? null
+    : fetch(`https://restcountries.com/v3.1/alpha?codes=${country.borders.join(",")}`)
+    .then(res => res.json())
+    .then(data => setCountryNames(data))
+    .catch(err => console.log(err));
   }, [country])
 
   return countryNames;
